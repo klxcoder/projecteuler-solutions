ce93b6157a31e0017324909400f107a90e257ed6
klxcoder <klxcoder@gmail.com>
2024-12-03 15:33:09 +0700

    isEven

--- solutions/2/sol.go
+++ solutions/2/sol.go
@@ -4,6 +4,11 @@ import "fmt"
 
 const LIMIT = 4_000_000
 
+func isEven(number int) bool {
+	return number%2 == 0
+}
+
 func main() {
-	fmt.Println(LIMIT)
+	fmt.Println(isEven(10))
+	fmt.Println(isEven(11))
 }
