c9a6a5bfd3c3b2004908d1c9b0b2708a41ee2e67
klxcoder <klxcoder@gmail.com>
2024-12-03 14:49:25 +0700

    get_fib_sequence

--- solutions/2/sol.py
+++ solutions/2/sol.py
@@ -3,5 +3,15 @@ LIMIT = 4_000_000
 def is_even(term):
   return term % 2 == 0
 
-print(is_even(10))
-print(is_even(11))
+def get_fib_sequence(limit):
+  fib_sequence = [1, 2]
+  while True:
+    first, second = fib_sequence[-2:]
+    term = first + second
+    if(term > limit):
+      break
+    fib_sequence.append(term)
+  return fib_sequence
+
+fib_sequence = get_fib_sequence(100)
+print(fib_sequence)
