e384a5499cb1f2abbf859989c92005e30860a8ee
klxcoder <klxcoder@gmail.com>
2024-12-03 16:32:22 +0700

    getSmaller

--- solutions/3/sol.go
+++ solutions/3/sol.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+const N = 600851475143;
+
+func getSmaller(n int, k int) int {
+  for n % k == 0 {
+		n = int(n/k)
+	}
+  return n;
+}
+
 func main() {
-	fmt.Print("Go OK")
+	fmt.Print(getSmaller(100, 2))
 }
