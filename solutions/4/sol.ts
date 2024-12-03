5a69496b80c9961373f09fd90c9f467e55c80914
klxcoder <klxcoder@gmail.com>
2024-12-03 17:19:58 +0700

    solution for problem 4 in Typescript

---
+++ solutions/4/sol.ts
@@ -0,0 +1,28 @@
+const isPalindromic = (n: number): boolean => {
+  const nStr: string = n.toString();
+  let start = 0;
+  let end = nStr.length - 1;
+  while (start < end) {
+    if (nStr[start] !== nStr[end]) {
+      return false;
+    }
+    start++;
+    end--;
+  }
+  return true;
+}
+
+const getLargestPalindromic: () => number = () => {
+  let largetPalindromic = 0;
+  for (let first = 999; first >= 100; first--) {
+    for (let second = first; second >= 100; second--) {
+      const product = first * second;
+      if (product > largetPalindromic && isPalindromic(product)) {
+        largetPalindromic = product;
+      }
+    }
+  }
+  return largetPalindromic;
+}
+
+console.log(getLargestPalindromic())
