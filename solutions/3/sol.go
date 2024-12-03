93a186a5def2002daa21fd1b1693846d4bd86365
klxcoder <klxcoder@gmail.com>
2024-12-03 16:35:27 +0700

    getLargestPrimeFactor

--- solutions/3/sol.go
+++ solutions/3/sol.go
@@ -2,15 +2,26 @@ package main
 
 import "fmt"
 
-const N = 600851475143;
+const N = 600851475143
 
 func getSmaller(n int, k int) int {
   for n % k == 0 {
 		n = int(n/k)
 	}
-  return n;
+  return n
+}
+
+func getLargestPrimeFactor(n int) int {
+  k := 2
+  for {
+    n = getSmaller(n, k)
+    if n == 1 {
+			return k
+		}
+    k++;
+  }
 }
 
 func main() {
-	fmt.Print(getSmaller(100, 2))
+	fmt.Print(getLargestPrimeFactor(100))
 }
