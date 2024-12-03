a58a461a30a2542d6b5fe9e319513d27e9732527
klxcoder <klxcoder@gmail.com>
2024-12-03 13:16:23 +0700

    getFibSequence

--- solutions/2/sol.js
+++ solutions/2/sol.js
@@ -1,15 +1,18 @@
 // const LIMIT = 4_000_000;
-const LIMIT = 100;
 
 const isEven = (term) => term % 2 === 0;
 
-const fibSequence = [1, 2];
+const getFibSequence = (limit) => {
+  const fibSequence = [1, 2];
 
-while (true) {
-  const [first, second] = fibSequence.slice(-2);
-  const term = first + second;
-  if (term > LIMIT) break;
-  fibSequence.push(term);
+  while (true) {
+    const [first, second] = fibSequence.slice(-2);
+    const term = first + second;
+    if (term > limit) break;
+    fibSequence.push(term);
+  }
+
+  return fibSequence;
 }
 
-console.log(fibSequence);
+console.log(getFibSequence(200));
