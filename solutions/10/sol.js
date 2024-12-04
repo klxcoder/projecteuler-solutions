6b1fa31241f2937ba6480c3e71def6955a2af3d0
klxcoder <klxcoder@gmail.com>
2024-12-04 12:30:41 +0700

    getPrimes

--- solutions/10/sol.js
+++ solutions/10/sol.js
@@ -1,13 +1,30 @@
 const LIMIT = 2_000_000
 
 const getPrimes = (limit) => {
-  const flags = new Array(limit).fill(false);
-  return flags;
+  const isPrimes = new Array(limit).fill(true);
+  isPrimes[0] = false;
+  isPrimes[1] = false;
+  for (let num = 2; num < limit; num++) {
+    if (isPrimes[num]) {
+      let next = num * 2;
+      while (next < limit) {
+        isPrimes[next] = false;
+        next += num;
+      }
+    }
+  }
+  const primes = [];
+  for (let index = 0; index < isPrimes.length; index++) {
+    if (isPrimes[index]) {
+      primes.push(index);
+    }
+  }
+  return primes;
 }
 
 const getSum = (primes) => primes.reduce((sum, prime) => sum + prime, 0);
 
-const primes = getPrimes(3);
+const primes = getPrimes(10);
 console.log(primes);
 // const sum = getSum([1, 2, 100]);
 // console.log(sum);
