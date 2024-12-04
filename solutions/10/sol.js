8212eabab22db7240b4f30ce3420440acf2d2999
klxcoder <klxcoder@gmail.com>
2024-12-04 12:24:08 +0700

    init flags

--- solutions/10/sol.js
+++ solutions/10/sol.js
@@ -1,10 +1,13 @@
 const LIMIT = 2_000_000
 
-const getPrimes = () => {
-
+const getPrimes = (limit) => {
+  const flags = new Array(limit).fill(false);
+  return flags;
 }
 
 const getSum = (primes) => primes.reduce((sum, prime) => sum + prime, 0);
 
-const sum = getSum([1, 2, 100]);
-console.log(sum);
+const primes = getPrimes(3);
+console.log(primes);
+// const sum = getSum([1, 2, 100]);
+// console.log(sum);
