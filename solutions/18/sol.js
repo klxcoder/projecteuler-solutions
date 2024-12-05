9bdda0df6e02f1a0992fde960c847ac6558798e3
klxcoder <klxcoder@gmail.com>
2024-12-05 15:15:35 +0700

    getAnswer

---
+++ solutions/18/sol.js
@@ -0,0 +1,12 @@
+const EXAMPLE_ARR = [
+  [3],
+  [7, 4],
+  [2, 4, 6],
+  [8, 5, 9, 3]
+]
+
+const getAnswer = (arr) => {
+  return arr;
+}
+const answer = getAnswer(EXAMPLE_ARR);
+console.log(answer);
