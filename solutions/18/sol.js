1d078946a1274364de587653792369edd2c7d572
klxcoder <klxcoder@gmail.com>
2024-12-05 15:22:34 +0700

    first col

--- solutions/18/sol.js
+++ solutions/18/sol.js
@@ -9,6 +9,11 @@ const getAnswer = (arr) => {
   const n = arr.length;
   const sumArr = new Array(n).fill(true).map(() => new Array(n).fill(0));
   sumArr[0][0] = arr[0][0];
+  for (let row = 1; row < n; row++) {
+    sumArr[row][0] = arr[row][0] + sumArr[row - 1][0];
+    //
+    //
+  }
   return sumArr;
 }
 const answer = getAnswer(EXAMPLE_ARR);
