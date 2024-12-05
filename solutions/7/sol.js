132ac07022164cd0d59125b4671c8069f75a4c8c
klxcoder <klxcoder@gmail.com>
2024-12-05 10:51:06 +0700

    solution for problem 7 in Javascript

--- solutions/7/sol.js
+++ solutions/7/sol.js
@@ -23,9 +23,7 @@ const getPrimeNumbers = (n, headPrimeNumbers) => {
   }
   return [...headPrimeNumbers, ...tailPrimeNumbers];
 }
-console.log(getPrimeNumbers(2, [2])); // [ 2, 3 ]
-console.log(getPrimeNumbers(4, [2, 3])); // [ 2, 3, 5, 7 ]
-console.log(getPrimeNumbers(8, [2, 3, 5, 7])); // [ 2, 3, 5, 7, 11, 13 ]
+
 const getAnswer = (k) => {
   let n = 2;
   let primeNumbers = [2];
@@ -35,5 +33,6 @@ const getAnswer = (k) => {
   }
   return primeNumbers[k - 1];
 }
-const answer = getAnswer(6);
+
+const answer = getAnswer(K);
 console.log(answer);
