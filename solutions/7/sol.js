73f22f84419b121d5b3662ae51cb6ed23527b94f
klxcoder <klxcoder@gmail.com>
2024-12-05 10:50:02 +0700

    getAnswer

--- solutions/7/sol.js
+++ solutions/7/sol.js
@@ -1,3 +1,5 @@
+const K = 10_001;
+
 /**
  * Get all prime numbers from 0 to n*2
  * @param {*} n 
@@ -21,7 +23,17 @@ const getPrimeNumbers = (n, headPrimeNumbers) => {
   }
   return [...headPrimeNumbers, ...tailPrimeNumbers];
 }
-console.log(getPrimeNumbers(2, [2]));
-console.log(getPrimeNumbers(4, [2, 3]));
-console.log(getPrimeNumbers(8, [2, 3, 5, 7]));
-console.log(getPrimeNumbers(16, [2, 3, 5, 7, 11, 13]));
+console.log(getPrimeNumbers(2, [2])); // [ 2, 3 ]
+console.log(getPrimeNumbers(4, [2, 3])); // [ 2, 3, 5, 7 ]
+console.log(getPrimeNumbers(8, [2, 3, 5, 7])); // [ 2, 3, 5, 7, 11, 13 ]
+const getAnswer = (k) => {
+  let n = 2;
+  let primeNumbers = [2];
+  while (primeNumbers.length < k) {
+    primeNumbers = getPrimeNumbers(n, primeNumbers);
+    n *= 2;
+  }
+  return primeNumbers[k - 1];
+}
+const answer = getAnswer(6);
+console.log(answer);
