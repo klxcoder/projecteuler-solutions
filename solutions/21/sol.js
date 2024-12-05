f32d04fd7bbf9526b51f2d00682cef0f18100e52
klxcoder <klxcoder@gmail.com>
2024-12-05 18:15:12 +0700

    getSum

--- solutions/21/sol.js
+++ solutions/21/sol.js
@@ -1,6 +1,28 @@
 const d = (n) => {
   const squareRoot = Math.floor(Math.sqrt(n));
-  return squareRoot;
+  const properDivisors = [1];
+  for (let num = 2; num < squareRoot; num++) {
+    if (n % num === 0) {
+      properDivisors.push(num);
+      properDivisors.push(n / num);
+    }
+  }
+  if (squareRoot * squareRoot === n) {
+    properDivisors.push(squareRoot)
+  } else {
+    if (n % squareRoot === 0) {
+      properDivisors.push(squareRoot);
+      properDivisors.push(n / squareRoot);
+    }
+  }
+  return properDivisors;
 }
 
-console.log(d(10));
+const getSum = (arr) => {
+  let sum = 0;
+  for (const num of arr) sum += num;
+  return sum;
+}
+
+console.log(getSum(d(220)));
+console.log(getSum(d(284)));
