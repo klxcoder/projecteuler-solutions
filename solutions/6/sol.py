630561506533fbed48a6c69664650a2e52ec6fe6
klxcoder <klxcoder@gmail.com>
2024-12-05 21:05:01 +0700

    square_of_sum

--- solutions/6/sol.py
+++ solutions/6/sol.py
@@ -4,4 +4,9 @@ def sum_of_squares(n):
     sum = sum + num * num
   return sum
 
+def square_of_sum(n):
+  sum = int(n * (n+1) / 2)
+  return sum * sum
+
 print(sum_of_squares(10))
+print(square_of_sum(10))
