7d3c61f36e827d6b5bacd67ad78f1e09dcbcf7d7
klxcoder <klxcoder@gmail.com>
2024-12-06 08:24:25 +0700

    getAnser

--- solutions/25/sol.go
+++ solutions/25/sol.go
@@ -4,6 +4,19 @@ import (
 	"fmt"
 )
 
+func getAnser(digit int) int {
+	a := 1
+	b := 1
+	for index := 0; index < digit; index++ {
+		c := a + b
+		fmt.Println(c)
+		a = b
+		b = c
+	}
+	return 0
+}
+
 func main() {
-	fmt.Println("Go OK")
+	answer := getAnser(20)
+	fmt.Println(answer)
 }
