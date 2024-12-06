b93455ceba051949408b71542fd7d34f72ae6fbf
klxcoder <klxcoder@gmail.com>
2024-12-06 08:43:26 +0700

    getFactorial

--- solutions/34/sol.go
+++ solutions/34/sol.go
@@ -4,6 +4,17 @@ import (
 	"fmt"
 )
 
+func getFactorial(n int) int {
+	result := 1
+	for num := 2; num <= n; num++ {
+		result *= num
+	}
+	return result
+}
+
 func main() {
-	fmt.Println("Go OK")
+	fmt.Println(getFactorial((1)))
+	fmt.Println(getFactorial((4)))
+	fmt.Println(getFactorial((5)))
+	fmt.Println(getFactorial((9)))
 }
