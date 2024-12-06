e908c3767d7ad560ead32d90f92a1834cb3411ff
klxcoder <klxcoder@gmail.com>
2024-12-06 14:04:09 +0700

    solution for problem 46 in Typescript

--- solutions/46/sol.ts
+++ solutions/46/sol.ts
@@ -7,11 +7,37 @@ const isComposite: (n: number) => boolean = (n) => {
   return false;
 }
 
+const isPerfectSquare: (n: number) => boolean = (n) => {
+  const squareRoot = Math.floor(Math.sqrt(n));
+  return squareRoot * squareRoot === n;
+}
+
+const getEquation: (n: number, primeNumbers: number[]) => string = (n, primeNumbers) => {
+  for (const primeNumber of primeNumbers) {
+    let result = n - primeNumber;
+    if (result % 2 === 0) {
+      result = Math.floor(result / 2);
+      if (isPerfectSquare(result)) {
+        return `${n} = ${primeNumber} + 2 x ${result} x ${result}`
+      }
+    }
+  }
+  return ''
+}
+
 const getAnswer: () => number = () => {
   let num = 5;
+  const primeNumbers = [2, 3]
   while (true) {
     if (isComposite(num)) {
-      return num;
+      const equation = getEquation(num, primeNumbers);
+      if (equation) {
+        console.log(equation)
+      } else {
+        return num;
+      }
+    } else {
+      primeNumbers.push(num)
     }
     num += 2;
   }
