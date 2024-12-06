ba08b76c7cb64b2c0081c050b5e6b303bd6778f0
klxcoder <klxcoder@gmail.com>
2024-12-06 09:54:27 +0700

    estimate range

--- solutions/34/sol.go
+++ solutions/34/sol.go
@@ -12,9 +12,24 @@ func getFactorial(n int) int {
 	return result
 }
 
+func getSumFactorialDigits(n int) int {
+	sum := 0
+	for {
+		if n == 0 {
+			return sum
+		}
+		sum += getFactorial(n % 10)
+		n = int(n / 10)
+	}
+}
+
 func main() {
-	fmt.Println(getFactorial((1)))
-	fmt.Println(getFactorial((4)))
-	fmt.Println(getFactorial((5)))
-	fmt.Println(getFactorial((9)))
+	fmt.Println(getSumFactorialDigits((9)))
+	fmt.Println(getSumFactorialDigits((99)))
+	fmt.Println(getSumFactorialDigits((999)))
+	fmt.Println(getSumFactorialDigits((9999)))
+	fmt.Println(getSumFactorialDigits((99999)))
+	fmt.Println(getSumFactorialDigits((999999)))  // 2177280
+	fmt.Println(getSumFactorialDigits((9999999))) // 2540160
+	// curious number < 9999999
 }
