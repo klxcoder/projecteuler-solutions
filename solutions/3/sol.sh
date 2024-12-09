106b87c57b88b367e91107e1cd70604b0c5a07c7
klxcoder <klxcoder@gmail.com>
2024-12-10 02:01:57 +0700

    better algorithm

--- solutions/3/sol.sh
+++ solutions/3/sol.sh
@@ -2,39 +2,26 @@
 
 LIMIT=600851475143
 
-get_primes() {
-  isPrimes=()
-  isPrimes[0]=0
-  isPrimes[1]=0
-  for (( i=2; i<=$1; i++ )); do
-    isPrimes[$i]=1
+get_smaller() {
+  n=$1
+  k=$2
+  while [ $((n%k)) -eq 0 ]; do
+    n=$(($n/$k))
   done
-  square_root=$(awk "BEGIN {print int(sqrt($1))}")
-  primes=()
-  for (( i=2; i<=$square_root; i++ )); do
-    if [ ${isPrimes[$i]} -eq 1 ]; then
-      for (( j=$i*$i; j<=$1; j+=i )); do
-        isPrimes[$j]=0
-      done
-    fi
-  done
-   for (( i=$1; i>=2; i-- )); do
-    if [ ${isPrimes[$i]} -eq 1 ]; then
-      primes+=($i)
-    fi
-  done
-  echo "${primes[@]}"
+  echo $n
 }
 
 get_answer() {
-  square_root=$(awk "BEGIN {print int(sqrt($1))}")
-  primes=($(get_primes $square_root))
-  for prime in ${primes[@]}; do
-    if [ $(( $1 % $prime )) -eq 0 ]; then
-      echo $prime
+  n=$1
+  k=2
+  while [ true ]; do
+    n=$(get_smaller $n $k)
+    if [ $n -eq 1 ]; then
+      echo $k
       break
-    fi  
+    fi
+    k=$(($k+1))
   done
 }
 
-get_answer 13195
+get_answer $LIMIT
