d09ab2fe6f451be9941a26ee24744c7e525d40d9
klxcoder <klxcoder@gmail.com>
2024-12-09 22:28:33 +0700

    get_recurring_cycle

--- solutions/26/sol.sh
+++ solutions/26/sol.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
 get_recurring_cycle() {
-  echo "get_recurring_cycle for number $1"
   declare -A flags
   remainder=1
   result="0."
+  index=1
   while true; do
     a=$((remainder * 10))
     if [ ${flags[$a]} ] || [ "$remainder" == "0" ]; then
+      if [ "$remainder" == "0" ]; then
+        echo "$1 $result 0"
+      else
+        length=$((index-${flags[$a]}))
+        echo "$1 $result $length"
+      fi
       break
     fi
-    flags[$a]=1
+    flags[$a]=$index
     digit=$(($a/$1))
     result="$result$digit"
     remainder=$(($a%$1))
-    echo "digit = $digit"
-    echo "remainder = $remainder"
+    index=$((index+1))
   done
-  echo "$result"
 }
 
 get_recurring_cycle 2
@@ -25,3 +29,7 @@ get_recurring_cycle 3
 get_recurring_cycle 4
 get_recurring_cycle 5
 get_recurring_cycle 6
+get_recurring_cycle 7
+get_recurring_cycle 8
+get_recurring_cycle 9
+get_recurring_cycle 10
