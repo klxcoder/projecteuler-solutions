04fa6366ea420ba8402f0026f00fc3b17c43e950
klxcoder <klxcoder@gmail.com>
2024-12-11 13:10:24 +0700

    is_palindromic

--- solutions/4/sol.sh
+++ solutions/4/sol.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+is_palindromic() {
+  echo $1
+}
+
 get_answer() {
   start=$1
   end=$2
   for ((i=$start; i<=$end; i++)) ; do
     for ((j=i; j<=end; j++)); do
       product=$(($i*$j))
-      echo $product
+      is_palindromic $product
     done
   done
 }
