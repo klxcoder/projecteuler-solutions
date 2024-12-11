72c09c9e012e311edefab2711eeffddf6e70f788
klxcoder <klxcoder@gmail.com>
2024-12-11 13:06:20 +0700

    2 for loops

--- solutions/4/sol.sh
+++ solutions/4/sol.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 get_answer() {
-  echo $1
+  start=$1
+  end=$2
+  for ((i=$start; i<=$end; i++)) ; do
+    for ((j=i; j<=end; j++)); do
+      echo $i $j
+    done
+  done
 }
 
-get_answer Hello
+get_answer 10 12
