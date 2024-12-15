e64f14c96a3363fe9bf9b508e85d1232dc64cd16
klxcoder <klxcoder@gmail.com>
2024-12-15 14:53:42 +0700

    problem 5, bash, gcd

--- solutions/5/sol.sh
+++ solutions/5/sol.sh
@@ -2,16 +2,24 @@
 
 # Greatest common divisor
 gcd() {
-
+  if [ $1 -eq 0 ]; then
+    echo $2
+    return 0
+  fi
+  if [ "$1" -gt "$2" ]; then
+    gcd $(( $1 % $2 )) "$2"
+  else
+    gcd $(( $2 % $1 )) "$1"
+  fi
 }
 
 # Least common multiple
 lcm() {
-
+  echo "ok"
 }
 
 get_answer() {
-  echo $1 "and" $2
+  gcd $1 $2
 }
 
-get_answer 100 999
+get_answer 10 12
