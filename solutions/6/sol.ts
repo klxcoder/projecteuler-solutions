e91fc46c1e98301d2589540d812f35e77e0d07bc
klxcoder <klxcoder@gmail.com>
2024-12-15 15:05:34 +0700

    problem 6, ts, sumOfSquares

---
+++ solutions/6/sol.ts
@@ -0,0 +1,9 @@
+const sumOfSquares: (n: number) => number = (n) => {
+  let sum = 0;
+  for (let num = 1; num <= n; num++) {
+    sum += num * num;
+  }
+  return sum;
+}
+
+console.log(sumOfSquares(10))
