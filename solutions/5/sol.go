5cc9255211c06b6660e418074ace8dedeb41601b
klxcoder <klxcoder@gmail.com>
2024-12-15 14:34:19 +0700

    problem 5, go, gcd

--- solutions/5/sol.go
+++ solutions/5/sol.go
@@ -4,6 +4,21 @@ import (
 	"fmt"
 )
 
+// Greatest common divisor
+func gcd(a int, b int) int {
+	if a == 0 {
+		return b
+	}
+	if a > b {
+		return gcd(a%b, b)
+	} else {
+		return gcd(b%a, a)
+	}
+}
+
+// Least common multiple
+// lcm
+
 func main() {
-	fmt.Print("Go OK")
+	fmt.Print(gcd(10, 12))
 }
