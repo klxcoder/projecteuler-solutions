1bbeeeb1e463c261caf19e40269413bdc4cccde7
klxcoder <klxcoder@gmail.com>
2024-12-15 14:12:03 +0700

    problem 4, bash, match example answer

--- solutions/4/sol.sh
+++ solutions/4/sol.sh
@@ -20,13 +20,22 @@ get_answer() {
   local end=$2
   local i
   local j
+  local product
+  local max_length=0
+  local max_product=0
   for ((i=$start; i<=$end; i++)) ; do
     for ((j=i; j<=end; j++)); do
       product=$(($i*$j))
       is_palindromic $product
-      echo $product $?
+      if [ $? -eq 1 ]; then
+        if [ $product -gt $max_product ]; then
+          max_product=$product
+          max_length=${#product}
+        fi
+      fi
     done
   done
+  echo $max_product
 }
 
-get_answer 10 12
+get_answer 10 99
