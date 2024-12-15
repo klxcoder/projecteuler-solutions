a36e5e29eb583282570f8999e19ff39f3ca9fe3a
klxcoder <klxcoder@gmail.com>
2024-12-15 15:12:25 +0700

    problem 6, go, sumOfSquares

--- solutions/6/sol.go
+++ solutions/6/sol.go
@@ -4,6 +4,14 @@ import (
 	"fmt"
 )
 
+func sumOfSquares(n int) int {
+	sum := 0
+	for num := 1; num <= n; num++ {
+		sum += num * num
+	}
+	return sum
+}
+
 func main() {
-	fmt.Print("Go OK")
+	fmt.Print(sumOfSquares(10))
 }
