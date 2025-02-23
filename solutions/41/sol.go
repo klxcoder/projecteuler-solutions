4a3b717e8e47db28449597d93a11519c983d19f8
klxcoder <klxcoder@gmail.com>
2025-02-24 05:33:16 +0700

    add isPrime function and print prime numbers in main for problem 41 solution

--- solutions/41/sol.go
+++ solutions/41/sol.go
@@ -32,9 +32,32 @@ func nextPermutation(arr []int) []int {
 	return arr
 }
 
+func isPrime(n int) bool {
+	if n < 2 {
+		return false
+	}
+	if n == 2 || n == 3 {
+		return true
+	}
+	if n%2 == 0 || n%3 == 0 {
+		return false
+	}
+	for i := 5; i*i <= n; i += 6 {
+		if n%i == 0 || n%(i+2) == 0 {
+			return false
+		}
+	}
+	return true
+}
+
 func main() {
 	// arr := [10]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 	arr := [5]int{1, 3, 5, 4, 2}
 	nextArr := nextPermutation(arr[:])
 	fmt.Print(nextArr) // [1 4 2 3 5]
+	for i := 1; i <= 20; i++ {
+		if isPrime(i) {
+			fmt.Println(i)
+		}
+	}
 }
