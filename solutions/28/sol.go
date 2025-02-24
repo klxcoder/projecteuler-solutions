58a10f3bb1c2b606db50ea19ac14199502db9a76
klxcoder <klxcoder@gmail.com>
2025-02-24 21:28:46 +0700

    add getAnswer function to calculate spiral diagonal numbers and update main to display results for n = 1, 3, 5, and 7

--- solutions/28/sol.go
+++ solutions/28/sol.go
@@ -4,6 +4,23 @@ import (
 	"fmt"
 )
 
+func getAnswer(n int) []int {
+	arr := []int{1}
+	k := 3
+	number := 1
+	for k <= n {
+		for i := 0; i < 4; i++ {
+			number += k - 1
+			arr = append(arr, number)
+		}
+		k += 2
+	}
+	return arr
+}
+
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println(getAnswer(1))
+	fmt.Println(getAnswer(3))
+	fmt.Println(getAnswer(5))
+	fmt.Println(getAnswer(7))
 }
