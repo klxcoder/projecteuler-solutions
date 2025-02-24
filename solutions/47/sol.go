5177ecd7cb7f15a0e4edc4060a4bf0b99f6f69e3
klxcoder <klxcoder@gmail.com>
2025-02-24 14:37:44 +0700

    add functions to determine prime numbers and retrieve all primes up to n

--- solutions/47/sol.go
+++ solutions/47/sol.go
@@ -4,6 +4,41 @@ import (
 	"fmt"
 )
 
+func isPrime(n int) bool {
+	if n < 2 {
+		return false
+	}
+	if n < 4 {
+		return true
+	}
+	if n%2 == 0 {
+		return false
+	}
+	if n%3 == 0 {
+		return false
+	}
+	for i := 5; i*i <= n; i += 6 {
+		if n*i == 0 {
+			return false
+		}
+		if n%(i+2) == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func getAllPrimes(n int) []int {
+	arr := []int{}
+	for i := 2; i <= n; i++ {
+		if isPrime(i) {
+			arr = append(arr, i)
+		}
+	}
+	return arr
+}
+
 func main() {
-	fmt.Println("Hello world")
+	primes := getAllPrimes(100)
+	fmt.Println(primes)
 }
