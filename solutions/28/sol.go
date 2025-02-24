d93917a4745bbf7db283e5431044cb39ae2dd567
klxcoder <klxcoder@gmail.com>
2025-02-24 21:31:13 +0700

    refactor getAnswer function to return the sum of spiral diagonal numbers and update main to display result for n = 1001

--- solutions/28/sol.go
+++ solutions/28/sol.go
@@ -4,23 +4,21 @@ import (
 	"fmt"
 )
 
-func getAnswer(n int) []int {
-	arr := []int{1}
+func getAnswer(n int) int {
+	sum := 1
 	k := 3
 	number := 1
 	for k <= n {
 		for i := 0; i < 4; i++ {
 			number += k - 1
-			arr = append(arr, number)
+			sum += number
 		}
 		k += 2
 	}
-	return arr
+	return sum
 }
 
 func main() {
-	fmt.Println(getAnswer(1))
-	fmt.Println(getAnswer(3))
 	fmt.Println(getAnswer(5))
-	fmt.Println(getAnswer(7))
+	fmt.Println(getAnswer(1001))
 }
