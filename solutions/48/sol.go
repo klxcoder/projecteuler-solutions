252a18b49fc44709bf4c530408d01d40967a7938
klxcoder <klxcoder@gmail.com>
2025-02-24 14:12:11 +0700

    implement getProduct and getAnswer functions for calculating modular products

--- solutions/48/sol.go
+++ solutions/48/sol.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+var K = int(math.Pow(10, 10))
+
+func getProduct(n int) int {
+	product := 1
+	for i := 0; i < n; i++ {
+		product = (product * n) % K
+	}
+	return product
+}
+
+func getAnswer(n int) int {
+	answer := 0
+	for i := 1; i <= n; i++ {
+		answer = (answer + getProduct(i)) % K
+	}
+	return answer
+}
 
 func main() {
-	fmt.Print("Hello world")
+	fmt.Println(getAnswer((10)))
+	fmt.Println(getAnswer((1000)))
 }
