fe9ce37b32757329b8b7d478a50936a5e538ad63
klxcoder <klxcoder@gmail.com>
2025-02-25 06:48:42 +0700

    implement triangle path sum calculation and print the maximum path sum

--- solutions/18/sol.py
+++ solutions/18/sol.py
@@ -9,6 +9,12 @@ n = len(arr)
 
 for i in range(1, n):
   arr[i][0] += arr[i-1][0]
+  for j in range(1, i):
+    arr[i][j] += max(arr[i-1][j-1],arr[i-1][j])
   arr[i][i] += arr[i-1][i-1]
 
-print(arr)
+answer = 0
+for i in range(n):
+  answer = max(answer, arr[n-1][i])
+
+print(answer)
