77fee2d6a51d0880ff36f60d6a8228dae2ea71ce
klxcoder <klxcoder@gmail.com>
2025-02-24 09:03:24 +0700

    refactor getBlue and getAnswer functions for improved readability and performance

--- solutions/100/sol.py
+++ solutions/100/sol.py
@@ -4,25 +4,25 @@ import math
 def getBlue(total):
   a = 2
   b = -2
-  c = -total*(total-1)
+  c = -total * (total - 1)
 
   discriminant = b**2 - 4*a*c
-  delta = math.floor(math.sqrt(discriminant))
+  delta = int(math.isqrt(discriminant))
 
-  if(discriminant != delta * delta):
+  if discriminant != delta * delta:
     return -1
 
-  blue = (-b + delta) / (2 * a)
-  return math.floor(blue)
+  blue = (-b + delta) // (2 * a)
+  return blue
 
 def getAnswer(total):
   while True:
     blue = getBlue(total)
     if blue == -1:
-      total = total + 1
+      total += 1
     else:
       return blue
 
 print(getAnswer(21))
-print(getAnswer(85+35))
-print(getAnswer(10**12+1)) # impossible
+print(getAnswer(85 + 35))
+print(getAnswer(10**12 + 1))  # impossible
