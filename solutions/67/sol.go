1cc9f9b1144dad119911fd4c3570585130882d7f
klxcoder <klxcoder@gmail.com>
2025-02-25 17:41:14 +0700

    refactor getAnswer function to return maximum path sum in triangle array

--- solutions/67/sol.go
+++ solutions/67/sol.go
@@ -4,13 +4,20 @@ import (
 	"fmt"
 )
 
-func getAnswer(arr [][]int) [][]int {
+func getAnswer(arr [][]int) int {
 	n := len(arr)
 	for i := 1; i < n; i++ {
 		arr[i][0] += arr[i-1][0]
+		for j := 1; j < i; j++ {
+			arr[i][j] += max(arr[i-1][j-1], arr[i-1][j])
+		}
 		arr[i][i] += arr[i-1][i-1]
 	}
-	return arr
+	answer := 0
+	for i := 0; i < n; i++ {
+		answer = max(answer, arr[n-1][i])
+	}
+	return answer
 }
 
 func main() {
