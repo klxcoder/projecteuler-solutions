d6c05e2e7ad6b2f0bd98492473ae5cb8e6fb59c4
klxcoder <klxcoder@gmail.com>
2025-02-25 08:55:15 +0700

    implement phi array calculation using the sieve method

--- solutions/69/sol.ts
+++ solutions/69/sol.ts
@@ -1,5 +1,12 @@
 const getPhiArr = (n: number) => {
-  const phiArr: number[] = Array(n + 1).fill(true).map((_, index) => index)
+  const phiArr: number[] = Array(n + 1).fill(true).map((_, index) => index);
+  for (let x = 2; x <= n; x++) {
+    if (phiArr[x] == x) {
+      for (let y = x; y <= n; y += x) {
+        phiArr[y] = phiArr[y] * (x - 1) / x
+      }
+    }
+  }
   return phiArr;
 }
 
