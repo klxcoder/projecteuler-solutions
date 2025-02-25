5256cbf9192cddbb2b02ce869a2d360b37504046
klxcoder <klxcoder@gmail.com>
2025-02-25 17:36:10 +0700

    add function to calculate cumulative sums in a triangle array

--- solutions/67/sol.go
+++ solutions/67/sol.go
@@ -4,6 +4,21 @@ import (
 	"fmt"
 )
 
+func getAnswer(arr [][]int) [][]int {
+	n := len(arr)
+	for i := 1; i < n; i++ {
+		arr[i][0] += arr[i-1][0]
+		arr[i][i] += arr[i-1][i-1]
+	}
+	return arr
+}
+
 func main() {
-	fmt.Println("Hello world")
+	arr := [][]int{
+		{3},
+		{7, 4},
+		{2, 4, 6},
+		{8, 5, 9, 3},
+	}
+	fmt.Println(getAnswer(arr))
 }
