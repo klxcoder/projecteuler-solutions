4ad7d83d4ea1fb0b8da0691e0f359342f3626c62
klxcoder <klxcoder@gmail.com>
2025-02-25 19:14:13 +0700

    add function to read the first line from a file and log it

--- solutions/42/sol.js
+++ solutions/42/sol.js
@@ -3,4 +3,13 @@ const getWordValue = (word) => {
   return word.split('').reduce((sum, char) => sum + char.charCodeAt(0) - A + 1, 0);
 }
 
+const fs = require("fs");
+
+function readFirstLine(filePath) {
+  const data = fs.readFileSync(filePath, "utf8");
+  return data.split("\n")[0] || null;
+}
+
+console.log(readFirstLine("words.txt"));
+
 console.log(getWordValue("SKY"));
